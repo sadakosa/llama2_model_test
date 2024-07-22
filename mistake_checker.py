c16364ce04e0bf5e1374c5e3d1eab28adaef660a
sadakosa <sabrinachowzbl@gmail.com>
2024-07-22 22:14:01 +0800

    mistake checker

--- mistake_checker.py
+++ mistake_checker.py
@@ -1,65 +1,52 @@
 from spellchecker import SpellChecker
-from global_methods import get_text_from_file, load_yaml_config, remove_punctuation
+import csv
+import string
 
 
 class MistakeChecker:
     def __init__(self):
         self.spell = SpellChecker()
+        self.word_list = self.__get_word_list()
         return
 
-    def check_if_mistake(self, string_input='something is happening here'):
-        # remove punctuation
-        parsed_string_input = remove_punctuation(string_input)
+    def check_if_mistake(self, text='something is happening here'):
+        # prepare text
+        parsed_text = self.__remove_punctuation(text)
+        word_array = parsed_text.split()
 
         # spellcheck
-        # if mistake, adjust for basic spelling
-        
-        # spellcheck
-
-        word_array = parsed_string_input.split()
         misspelled = self.spell.unknown(word_array)
         print(misspelled)
+
         if len(misspelled) > 0:
             return True
         else:
             return False
-    
-    def add_spaces(concatenated_text, word_list):
-        # Initialize an empty list to hold the words
-        words = []
-        i = 0
-        while i < len(concatenated_text):
-            for word in word_list:
-                # Check if the word matches the current position in the concatenated text
-                if concatenated_text.startswith(word, i):
-                    words.append(word)
-                    i += len(word)
-                    break
-            else:
-                # If no word matches, just move to the next character
-                i += 1
 
-        # Join the words with spaces
-        spaced_text = ' '.join(words)
-        return spaced_text
+    # def correct_if_mistake(self, text='something is happening here'):
+    #     # if mistake, adjust for basic spelling
+    #     is_mistake = self.check_if_mistake(text)
+    #     if is_mistake:
+    #         # google did you mean
+    #         # check if still mispelled
+    #         # if yes, return yes to direct it to groq
+    #     else:
+    #         return text
 
-    # Example usage
-    concatenated_text = "seenartificialintelligenceis"
-    word_list = ["seen", "artificial", "intelligence", "is"]
-    spaced_text = add_spaces(concatenated_text, word_list)
-    print(spaced_text)
+    # ================== PRIVATE METHODS ==================
+    def __remove_punctuation(self, text):
+        return ''.join([char for char in text if char not in string.punctuation])
+
+    def __get_word_list(self):
+        csv_file_path = 'resources/word_list.csv'
+        word_list = []
+
+        with open(csv_file_path, mode='r') as file: # open the csv file, read
+            csv_reader = csv.reader(file)
+            
+            for row in csv_reader:
+                word_list.append(row)
+        return word_list
 
 
-    def correct_mistake(self, string_input='something is happening here'):
-        word_array = string_input.split()
-        misspelled = self.spell.unknown(word_array)
-        
-        for word in misspelled:
-            # Get the one `most likely` answer
-            print(self.spell.correction(word))
-
-            # Get a list of `likely` options
-            print(self.spell.candidates(word))
-        
-        return
 
